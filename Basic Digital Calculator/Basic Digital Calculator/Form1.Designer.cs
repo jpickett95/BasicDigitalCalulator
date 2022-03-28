00d7ffdefa61df85a010ff0e71c1d4c49af72b2b
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-03-28 14:52:08 -0400

    Added CurrentEntry variable & functionality for DisplayPanel to show the current entry;

--- Basic Digital Calculator/Basic Digital Calculator/Form1.Designer.cs
+++ Basic Digital Calculator/Basic Digital Calculator/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace Basic_Digital_Calculator
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.ButtonsPanel = new System.Windows.Forms.TableLayoutPanel();
             this.DisplayPanel = new System.Windows.Forms.Panel();
@@ -57,6 +58,7 @@ namespace Basic_Digital_Calculator
             this.PeriodButton = new System.Windows.Forms.Button();
             this.EqualsButton = new System.Windows.Forms.Button();
             this.FormulaStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.DeleteToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.ButtonsPanel.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -105,10 +107,12 @@ namespace Basic_Digital_Calculator
             // 
             // DisplayPanel
             // 
+            this.DisplayPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.DisplayPanel.Location = new System.Drawing.Point(12, 12);
             this.DisplayPanel.Name = "DisplayPanel";
             this.DisplayPanel.Size = new System.Drawing.Size(394, 72);
             this.DisplayPanel.TabIndex = 1;
+            this.DisplayPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DisplayPanel_Paint);
             // 
             // ModuloDivsionButton
             // 
@@ -142,6 +146,7 @@ namespace Basic_Digital_Calculator
             // 
             // DeleteButton
             // 
+            this.DeleteButton.AccessibleDescription = "Delete";
             this.DeleteButton.AccessibleName = "Delete";
             this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DeleteButton.Image = ((System.Drawing.Image)(resources.GetObject("DeleteButton.Image")));
@@ -149,6 +154,8 @@ namespace Basic_Digital_Calculator
             this.DeleteButton.Name = "DeleteButton";
             this.DeleteButton.Size = new System.Drawing.Size(94, 52);
             this.DeleteButton.TabIndex = 3;
+            this.DeleteButton.Tag = "Delete";
+            this.DeleteToolTip.SetToolTip(this.DeleteButton, "Delete");
             this.DeleteButton.UseVisualStyleBackColor = true;
             // 
             // OneDividedByEntryButton
@@ -406,6 +413,7 @@ namespace Basic_Digital_Calculator
         private System.Windows.Forms.Button EqualsButton;
         private System.Windows.Forms.Panel DisplayPanel;
         private System.Windows.Forms.StatusStrip FormulaStatusStrip;
+        private System.Windows.Forms.ToolTip DeleteToolTip;
     }
 }
 
--- Basic Digital Calculator/Basic Digital Calculator/Form1.cs
+++ Basic Digital Calculator/Basic Digital Calculator/Form1.cs
@@ -12,9 +12,22 @@ namespace Basic_Digital_Calculator
 {
     public partial class Form1 : Form
     {
+        public long CurrentEntry = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void DisplayPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Font font = new Font("Microsoft Sans Serif", 30);
+
+            StringFormat stringFormat = new StringFormat();
+            stringFormat.Alignment = StringAlignment.Far;
+            stringFormat.LineAlignment = StringAlignment.Far;
+
+            e.Graphics.DrawString(CurrentEntry.ToString(), font, Brushes.Black, DisplayPanel.ClientRectangle, stringFormat);
         }
     }
 }
